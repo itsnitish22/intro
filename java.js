9035dfa1e9028b936b321232dd6653b34cb7ee53
Aniket Khajanchi <67803800+Aniketk257@users.noreply.github.com>
2020-07-04 20:37:27 +0530

    Update java.js

--- java.js
+++ java.js
@@ -2,4 +2,5 @@ var fn=prompt("What is your first Name?");
 var sn=prompt("What is your second Name?");
 var age=prompt("What is your age");
 alert("Your full name is"+fn+" "+sn);
-alert("You are"+age+" years old");
+alert("Hurray!
+       You are "+age+" years old");
