8804b9345afd5370c3e3ebbf3fc4e820ae557d70
Aniket Khajanchi <67803800+Aniketk257@users.noreply.github.com>
2020-07-04 20:49:36 +0530

    Delete java.js

--- java.js
+++
@@ -1,6 +0,0 @@
-var fn=prompt("What is your first Name?");
-var sn=prompt("What is your second Name?");
-var age=prompt("What is your age");
-alert("Your full name is"+fn+" "+sn);
-alert("Hurray!
-       You are "+age+" years old");
